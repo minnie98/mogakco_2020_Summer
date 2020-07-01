f4ab748f526c0247c97d803a9d829ae1ee8c5e93
mini98 <44043938+minnie98@users.noreply.github.com>
2020-07-01 17:16:32 +0900

    Update NandM1.py

--- NandM1.py
+++ NandM1.py
@@ -1,17 +1,17 @@
 N, M = map(int, input().split())
 visited = [False for i in range(N)]
 out = []
-
+#DFS algorithm
 def N_M(depth, N, M):
     if depth == M:
-        print(' '.join(map(str, out)))
+        print(' '.join(map(str, out))) #output
         return
     for i in range(N):
-        if not visited[i]:
-            visited[i] = True
-            out.append(i+1)
-            N_M(depth + 1, N, M)
-            visited[i] = False
+        if not visited[i]: #Explore for Unvisited Nodes
+            visited[i] = True #Check visits
+            out.append(i+1) #Visit Content
+            N_M(depth + 1, N, M) #Recursive
+            visited[i] = False #Visit Completed
             out.pop()
 
 N_M(0, N, M)
